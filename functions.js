33dbf93eba131d06db876c1cd9b7490cf67ad2b6
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2023-12-19 17:58:09 -0500

    void type

--- functions.js
+++ functions.js
@@ -9,3 +9,10 @@ greet("Shuklao");
 function square(num) {
     return num * num;
 }
+//void return type
+function printTwice(name) {
+    console.log(name);
+    console.log(name);
+    // return true
+    //if we uncomment the above line we'll get an error since we have annotated the return as void for this function but now we're trying to return a boolean instead
+}
--- functions.ts
+++ functions.ts
@@ -11,3 +11,11 @@ greet("Shuklao")
 function square(num : number) : number{
     return num*num
 }
+
+//void return type
+function printTwice(name): void{
+    console.log(name)
+    console.log(name)
+    // return true
+    //if we uncomment the above line we'll get an error since we have annotated the return as void for this function but now we're trying to return a boolean instead
+}
