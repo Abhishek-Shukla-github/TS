5f4c5cf3c667410210810fd34c28c3aa626dbf7e
abhishek-shukla-github <abhishekshuklaJS@gmail.com>
2023-12-20 00:46:31 -0500

    Combingin Alias

--- objects.ts
+++ objects.ts
@@ -6,7 +6,17 @@ type Person = {
     race?: string    //Optional property is denoted with ? 
 }
 
+type Profession = {
+    job: string
+}
+
+type LinkedIn = Person & Profession  //Combining 2 types
+
 function printPerson(person: Person):string{
+    return `Hi ,name: ${person.fullName} and I'm ${person.age} and i'm from ${person.place}`
+}
+
+function printLinkedin(person: LinkedIn):string{
     return `Hi ,name: ${person.fullName} and I'm ${person.age} and i'm from ${person.place}`
 }
 
@@ -14,5 +24,11 @@ printPerson({
     fullName: "Abhishek Shukla",
     age: 24,
     place: "Montreal"
+})
+printLinkedin({
+    fullName: "Abhishek Shukla",
+    age: 24,
+    place: "Montreal",
+    job: "SE"
 })
 
